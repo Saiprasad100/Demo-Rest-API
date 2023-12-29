f25cce75a92a28ea7df37b7e520b904249e3af7b
Saiprasad100 <121687380+Saiprasad100@users.noreply.github.com>
2023-12-29 21:08:12 +0530

    Update Application.java

--- src/main/java/in/sai/Application.java
+++ src/main/java/in/sai/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+String S= "saiprasad";
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
